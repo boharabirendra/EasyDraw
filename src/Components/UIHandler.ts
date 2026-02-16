01300bc29d8fe4665454e15e10f0de0a8722df7b
boharabirendra <boharabirendra546@gmail.com>
2026-02-16 23:22:49 +0545

    fix(AI-1): Fix typo in button

--- src/Components/UIHandler.ts
+++ src/Components/UIHandler.ts
@@ -7,7 +7,7 @@ export function handleShortcut() {
   ) as HTMLDivElement;
   shortcutItemsEl.style.display = "none";
   let flag = false;
-  const shortcutEl = document.querySelector("#shortcut") as HTMLButtonElement;
+  const shortcutEl = document.querySelector("#shortcut") as HTMLButtonElement; shortcutEl.textContent = 'Shortcuts';
   shortcutEl.addEventListener("click", () => {
     if (flag) {
       shortcutItemsEl.style.display = "none";
